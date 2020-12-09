a00eb4602001fbd14cbc43870fe03d5e343a6da7
lamamalaga <tomchuk.01@gmail.com>
2020-12-09 21:32:22 +0500

    Здание с использованием делегатов и дженериков.

---
+++ PhotoEnhancer2/PhotoEnhancer/Filters/SepiaParameters.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhotoEnhancer
+{
+    class SepiaParameters : IParameters
+    {
+        public double SepiaParameterHueByUser { get; set; }
+        public double SepiaParameterSaturationByUser { get; set; } 
+        public ParameterInfo[] GetDescription()
+        {
+            return new[]
+           {
+                new ParameterInfo() {
+                    Name = "Оттенок",
+                    MinValue = 0,
+                    MaxValue = 359.95,
+                    DefailtValue = 40,
+                    Increment = 0.05
+                },
+
+                new ParameterInfo()
+                {
+                    Name = "Насыщенность",
+                    MinValue = 0,
+                    MaxValue = 1,
+                    DefailtValue = 0.2,
+                    Increment = 0.01
+                }
+            };
+        }
+
+        public void SetValues(double[] values)
+        {
+            SepiaParameterHueByUser = values[0];
+            SepiaParameterSaturationByUser = values[1];
+        }
+    }
+}
