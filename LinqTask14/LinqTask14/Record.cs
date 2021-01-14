127ceab32560c38eb981041c611257824c542f34
lamamalagа <tomchuk.01@gmail.com>
2021-01-14 08:40:43 +0500

    Задание 3, 14 вариант

---
+++ LinqTask14/LinqTask14/Record.cs
@@ -0,0 +1,10 @@
+﻿namespace LinqTask14
+{
+    public struct Record
+    {
+        public int ClientID; // идентификационный номер клиента 
+        public int Year;     // год 
+        public int Month;    // номер месяца 
+        public int Duration; // продолжительность занятий в данном месяце // данного года (в часах) }
+    }
+}
